c9a2bdcb0139ab2ba83cebe86bbaf649ea51d274
Md.Oly Ullah <136308270+Mdolyullah@users.noreply.github.com>
2023-09-07 22:46:06 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// text-change
+function textChange(){
+document.getElementById('h2').innerHTML="Hello world";
+
+}
+// text-change
+function textChange1(){
+document.getElementById('h3').innerHTML="Hello world";
+
+}
+// text-change
+function textChange2(){
+document.getElementById('h4').innerHTML="Hello world";
+
+}
+// text-change
+function textChange3(){
+document.getElementById('h5').innerHTML="Hello world";
+
+}
+// text-change
+function textChange4(){
+document.getElementById('h6').innerHTML="Hello world";
+
+}
+// text-change
+function textChange5(){
+document.getElementById('h7').innerHTML="Hello world";
+
+}
+// text-change
+function textChange6(){
+document.getElementById('h8').innerHTML="Hello world";
+
+}
+// text-change
+function textChange7(){
+document.getElementById('h9').innerHTML="Hello world";
+
+}
+// text-change
+function textChange8(){
+document.getElementById('h10').innerHTML="Hello world";
+
+}
+// text-change
+function textChange9(){
+document.getElementById('h11').innerHTML="Hello world";
+
+}
